ac5609043989267c197ef5de2bb6679c812b94bf
nakamura <nakamura@lemon34.local>
2016-10-28 10:16:56 +0900

    斜方投射で，任意の位置にある標的を撃墜するパラメータを探索するプログラム

---
+++ ShootingDown.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <math.h>
+
+#define N 4000
+#define M 5
+#define T 4
+#define pi M_PI
+
+double dist(double a, double b){
+    double x = a * a + b * b;
+    
+    return sqrt(x);
+}
+
+void judgement(double distance, double radius){
+    printf("Distance of closest approach = %lf\n", distance);
+    
+    if(distance > radius){
+        printf("Fail!\n");
+    }else{
+        printf("Success!\n");
+    }
+}
+
+int main(void){
+    int i, j;
+    int k = 0;
+    double dt = (double)T / N;
+    double xN, xN_new;
+    double yN, yN_new;
+    double uN, uN_new;
+    double vN, vN_new;
+    double m = 1.0;
+    double gamma = 0.1;
+    double g = 9.8;
+    double radius = 1.0;
+    double distance;
+    double distance_c = dist(15.0, 20.0);
+    double theta, flag;
+    
+    double v0;
+    printf("v0 = ");
+    scanf("%lf", &v0);
+    
+    for(i = 0; i <= 90; ++i){
+        theta = i * pi / (2.0 * 90.0);
+        flag = dist(15.0, 20.0);
+        
+        xN = 0.0;
+        yN = 0.0;
+        uN = v0 * cos(theta);
+        vN = v0 * sin(theta);
+    
+        for(j = 0; j < N; ++j){
+            distance = dist(xN - 20.0, yN - 15.0);
+        
+            if(flag > distance){
+                flag = distance;
+            }
+        
+            xN_new = xN + dt * uN;
+            yN_new = yN + dt * vN;
+            uN_new = uN - dt * (gamma / m) * uN;
+            vN_new = vN - dt * (g + (gamma / m) * vN);
+            xN = xN_new;
+            yN = yN_new;
+            uN = uN_new;
+            vN = vN_new;
+        }
+        
+        if(distance_c > flag){
+            distance_c = flag;
+            k = i;
+        }
+    }
+    
+    printf("v0 = %lf,theta = %d * (pi / 2)\n", v0, k);
+    judgement(distance_c, radius);
+    
+    return 0;
+}
